80516b9abb4d0302a58a534a73d246dc9e436909
ana-puskaric <ana.puskaric99@gmail.com>
2025-07-02 18:32:51 +0200

    added search to homepage

--- my-react-app/src/home.jsx
+++ my-react-app/src/home.jsx
@@ -1,10 +1,14 @@
 import './home.css'
+import {Link} from 'react-router-dom'
 
 export default function Home(){
     return(
         <div className="home-container">
-            <button className="home-page-btn">Movies</button>
-            <button className="home-page-btn">AddMovie</button>
+            <div className="buttonas-wrapper">
+                <Link to="/movies"><button className="home-page-btn">Movies</button></Link>
+                <Link to="/add"><button className="home-page-btn">AddMovie</button></Link>
+            </div>
+            <input className="search" type="text" placeholder="Search movie..." />
         </div>
     );
 }
