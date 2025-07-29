d513c47156b30cae9f3499313d559299bc1da97b
Katarina Živković <kzivkovic230@gmail.com>
2025-07-29 20:35:55 +0200

    Add .env file

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -1,19 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
 
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  console.log('Loaded env:', env)
 
-dotenv.config()
-
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5172,
-    proxy: {
-      '/api': {
-        target: 'https://bootcamp2025grupa1.onrender.com',  // bez /api na kraju
-        changeOrigin: true,
-        secure: false,
+  return {
+    plugins: [react()],
+    server: {
+      port: 5172,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL ? env.VITE_API_URL.replace('/api', '') : '',
+          changeOrigin: true,
+          secure: false,
+        }
       }
     }
   }
