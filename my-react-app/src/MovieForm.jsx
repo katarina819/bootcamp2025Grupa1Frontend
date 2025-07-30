3dc3ed592ddf0abe14930a804f82531f8b4b5074
Katarina Živković <kzivkovic230@gmail.com>
2025-07-30 16:53:21 +0200

    Update fetch genres

--- my-react-app/src/MovieForm.jsx
+++ my-react-app/src/MovieForm.jsx
@@ -39,11 +39,11 @@ function MovieForm({ addMovie, updateMovie }) {
   useEffect(() => {
     const fetchGenreAndLanguage = async () => {
       try {
-        const genresResponse = await axios.get(`${API_URL}/genres`);
-        setAvailableGenres(genresResponse.data.items || genresResponse.data);
-        const languagesResponse = await axios.get(`${API_URL}/language`);
+        const genreResponse = await axios.get(`${API_URL}/genres`);
+        setAvailableGenres(genreResponse.data.items || genreResponse.data);
+        const languageResponse = await axios.get(`${API_URL}/language`);
         setAvailableLanguages(
-          languagesResponse.data.items || languagesResponse.data
+          languageResponse.data.items || languageResponse.data
         );
       } catch (error) {
         console.error("Error fetching genres or languages:", error);
