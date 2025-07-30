57a4ed441c0e4f40d7ab63f843902870aa6573d3
Katarina Živković <kzivkovic230@gmail.com>
2025-07-30 23:48:00 +0200

    Update delete

--- my-react-app/src/delete.jsx
+++ my-react-app/src/delete.jsx
@@ -5,7 +5,7 @@ const API_URL = import.meta.env.VITE_API_URL;
 
 export async function DeleteMovie(id){
     try {
-    await axios.delete(`${API_URL}/Movie/${id}`);
+    await axios.delete(`${API_URL}/api/movie/${id}`);
   } catch (err) {
     throw new Error(err.response?.data || err.message);
   }
